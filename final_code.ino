0904fdde9b81fe86748a02f216acfecdbe91db3a
dominicding <xding@cca.edu>
2016-12-14 15:03:01 -0800

    Add files via upload

---
+++ final_code.ino
@@ -0,0 +1,67 @@
+
+#include <AFMotor.h> 
+#define trigPin 10
+#define echoPin 11
+AF_DCMotor motor1(1, MOTOR12_8KHZ);
+AF_DCMotor motor2(2, MOTOR12_8KHZ);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Motor test!");
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+ }
+
+void forward() {
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+}
+void backward() {
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+}
+
+void loop() {
+
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
+  if (distance < 25) { /*if there's an obstacle 25 centimers, ahead, do the following: */
+    Serial.println ("Close Obstacle detected!" );
+    Serial.println ("Obstacle Details:");
+    Serial.print ("Distance From Robot is " );
+    Serial.print ( distance);
+    Serial.print ( " CM!");
+
+    Serial.println (" The obstacle is declared a threat due to close distance. ");
+    Serial.println (" Turning !");
+    backward();
+
+  }
+  else {
+    Serial.println ("No obstacle detected. going forward");
+    delay (5);
+   forward();
+  }
+
+
+
+
+
+
+
+}
